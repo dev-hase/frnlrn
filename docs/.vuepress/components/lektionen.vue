3c214aad2cc0800a2af591568f767099ae77b19c
Daniel Schmidt <2wusel@web.de>
2021-03-18 16:41:36 +0100

    vh slabtext sizing

--- docs/.vuepress/components/lektionen.vue
+++ docs/.vuepress/components/lektionen.vue
@@ -3,93 +3,95 @@
      
       <router-link :to="'/lektion01'" id="l1">
       	<div id="l1w">
-      	<span class="slabtext" id="l1a">Persönliche</span>
-      	<span class="slabtext" id="l1b">Voraussetzungen</span>
+      	<span id="l1a">Persönliche</span>
+      	<span id="l1b">Voraussetzungen</span>
         </div>
       </router-link>  
      
       <router-link :to="'/lektion02'" id="l2">
       	<div id="l2w">
-        <span class="slabtext" id="l2a">Risikofaktor</span> 
-        <span class="slabtext" id="l2b">Mensch</span>
+        <span id="l2a">Risikofaktor</span> 
+        <span id="l2b">Mensch</span>
         </div>
       </router-link>  
      
       <router-link :to="'/lektion03'" id="l3">
 		<div id="l3w">
-      	<span class="slabtext" id="l3a">Rechtliche</span>
-      	<span class="slabtext" id="l3b">Rahmenbedingungen</span>
+      	<span id="l3a">Rechtliche</span>
+      	<span id="l3b">Rahmen-</span>
+      	<span id="l3c">bedingungen</span>
         </div>
       </router-link>  
      
       <router-link :to="'/lektion04'" id="l4">
  		<div id="l4w">
-      	<span class="slabtext" id="l4a">Straßen-</span>
-      	<span class="slabtext" id="l4b">verkehrssystem</span>
-      	<span class="slabtext" id="l4c">& seine Nutzung</span>
+      	<span id="l4a">Straßen-</span>
+      	<span id="l4b">verkehrs-</span>
+      	<span id="l4c">system</span>
+      	<span id="l4d">& seine Nutzung</span>
         </div>       
       </router-link>  
      
       <router-link :to="'/lektion05'" id="l5">
         <div id="l5w">
-      	<span class="slabtext" id="l5a">Vorfahrt &</span>
-      	<span class="slabtext" id="l5b">Verkehrsregelungen</span>
+      	<span id="l5a">Vorfahrt &</span>
+      	<span id="l5b">Verkehrsregelungen</span>
         </div>     
       </router-link>  
      
       <router-link :to="'/lektion06'" id="l6">
         <div id="l6w">
-      	<span class="slabtext" id="l6a">Verkehrszeichen &</span>
-      	<span class="slabtext" id="l6b">Verkehrseinrichtungen</span>
-      	<span class="slabtext" id="l6c">+ Bahnübergänge</span>
+      	<span id="l6a">Verkehrszeichen &</span>
+      	<span id="l6b">Verkehrseinrichtungen</span>
+      	<span id="l6c">+ Bahnübergänge</span>
         </div>   
       </router-link>  
      
       <router-link :to="'/lektion07'" id="l7">
         <div id="l7w">
-      	<span class="slabtext" id="l7a">Andere Teilnehmer</span>
-      	<span class="slabtext" id="l7b">im Straßenverkehr</span>
+      	<span id="l7a">Andere Teilnehmer</span>
+      	<span id="l7b">im Straßenverkehr</span>
         </div>  
       </router-link>  
      
       <router-link :to="'/lektion08'" id="l8">
         <div id="l8w">
-      	<span class="slabtext" id="l8a">Geschwindigkeit,</span>
-      	<span class="slabtext" id="l8b">Abstand &</span>
-      	<span class="slabtext" id="l8c">umweltschonende Fahrweise</span>
+      	<span id="l8a">Geschwindigkeit,</span>
+      	<span id="l8b">Abstand &</span>
+      	<span id="l8c">umweltschonende Fahrweise</span>
         </div>   
       </router-link>  
      
       <router-link :to="'/lektion09'" id="l9">
         <div id="l9w">
-      	<span class="slabtext" id="l9a">Verkehrsverhalten</span>
-      	<span class="slabtext" id="l9b">...bei Fahrmanövern,</span>
-      	<span class="slabtext" id="l9c">Verkehrsbeobachtung</span>
+      	<span id="l9a">Verkehrsverhalten</span>
+      	<span id="l9b">...bei Fahrmanövern,</span>
+      	<span id="l9c">Verkehrsbeobachtung</span>
         </div>  
       </router-link>  
      
       <router-link :to="'/lektion10'" id="l10">
         <div id="l10w">
-      	<span class="slabtext" id="l10a">Ruhender</span>
-      	<span class="slabtext" id="l10b">Verkehr</span>
+      	<span id="l10a">Ruhender</span>
+      	<span id="l10b">Verkehr</span>
         </div>  
       </router-link>  
      
       <router-link :to="'/lektion11'" id="l11">
            
         <div id="l11w">
-      	<span class="slabtext" id="l11a">Verhalten in</span>
-      	<span class="slabtext" id="l11b">besonderen Situationen,</span>
-      	<span class="slabtext" id="l11c">Folgen von</span>
-      	<span class="slabtext" id="l11d">Verstößen gegen</span>
-      	<span class="slabtext" id="l11e">Verkehrsvorschriften</span>
+      	<span id="l11a">Verhalten in</span>
+      	<span id="l11b">besonderen Situationen,</span>
+      	<span id="l11c">Folgen von</span>
+      	<span id="l11d">Verstößen gegen</span>
+      	<span id="l11e">Verkehrsvorschriften</span>
         </div>  
       </router-link>  
      
       <router-link :to="'/lektion12'" id="l12">
         <div id="l12w">
-      	<span class="slabtext" id="l12a">Lebenslanges</span>
-      	<span class="slabtext" id="l12b">Lernen</span>
+      	<span id="l12a">Lebenslanges</span>
+      	<span id="l12b">Lernen</span>
         </div>  
       </router-link>  
 
@@ -98,17 +100,6 @@
 
 <script>
 
-import VanillaSlab from 'vanilla-slab';
-
-export default {
-  mounted() {
-    var mySlab = new VanillaSlab;
-	mySlab.init({
-	  selector: '.slabtext',
-	});
-
-  }
-};
 </script>
 
 <style lang="stylus" scoped>
@@ -117,30 +108,34 @@ export default {
 
 	display grid
 	grid-template-columns 1fr 1fr
-	grid-gap 1.5rem
+	grid-gap 4vw
 	font-family $fontB
 
 	@media (max-width $MQMobile)
 		grid-template-columns 1fr
-		word-break keep-all
-		white-space nowrap
+		//word-break keep-all
+		//white-space nowrap
 
 
 	@media (min-width 1200px)
 		grid-template-columns 1fr 1fr 1fr
-		grid-gap 3vw
 
 	a 
 		display flex
 		justify-content center
 		flex-direction column
+		align-items center
+
 		transition .3s
 		overflow hidden
-		//padding 2rem
-		height 20rem
+		//padding .5rem
+		min-height 40vh
+		//min-width 28vw
 		border-bottom-left-radius 1.25rem
 		border-bottom-right-radius .25rem
 		border-top-right-radius .5rem
+
+		font-size 3.25em
 
 		@media (max-width $MQMobile)
 			//min-height 15rem
@@ -182,11 +177,12 @@ export default {
 
 #l1a
 	transform rotate(3 deg)
-
+	font-size 8.25vh
 
 #l1b
 	background #dd254d
 	line-height 1.6 !important
+	font-size 5.25vh
 
 // -------------------------------------------
 
@@ -220,12 +216,14 @@ export default {
 	margin-bottom -2rem
 	text-decoration underline
 	font-weight 300
+	font-size 9vh
 
 #l2b
 
 	text-decoration none
 	color #e53512
 	text-shadow 3px 4px 5px #333
+	font-size 14vh
 
 // -------------------------------------------
 
@@ -254,21 +252,30 @@ export default {
 	justify-content center
 	flex-direction column
 	transform rotate(-6deg)
+	padding .5rem
 
 
 #l3a
 
-	margin-bottom 2rem
 	text-transform uppercase
 	font-family $fontA 
 	color #eee
+	font-size 7vh
+	margin-bottom -.5rem
+	transform rotate(5deg)
 
-#l3b
+#l3b, #l3c
 
 	text-decoration none
 	color #aaa
 	font-family $fontB
-	text-decoration overline 
+	font-size 12vh
+	background #333
+	padding .5rem
+	margin -.52rem
+
+#l3c
+	font-size 8vh
 
 // -------------------------------------------
 
@@ -296,24 +303,40 @@ export default {
 #l4w
 	display flex
 	justify-content center
+	align-items center
 	flex-direction column
 	transform rotate(3deg)
 
 #l4a
 	transform rotate(-1deg)
 
-#l4a, #l4b
+#l4a, #l4b, #l4c
 
 	margin-bottom 1rem
 	color #701358
 	font-family $fontC
-	letter-spacing .9rem
+	letter-spacing .1rem
+
+#l4a
+	font-size 11.5vh
+
+#l4b
+	font-size 10.75vh
+	margin-top -2rem
 
 #l4c
+	font-size 14.75vh
+	margin-top -3rem
+#l4d
 
 	text-decoration none
-	color #e53512
+	color #e2cc5d
 	text-shadow 3px 3px 5px #333
+	margin-bottom 2rem
+	font-size 6vh
+	background #701358
+	padding .5rem .25rem
+	transform rotate(-4.5deg)
 
 // -------------------------------------------
 
