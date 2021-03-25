cc27176780374855e0fd484b24e0c7523f4f9370
Daniel Schmidt <2wusel@web.de>
2021-03-25 16:28:51 +0100

    disaster

--- docs/.vuepress/components/fullPage.vue
+++ docs/.vuepress/components/fullPage.vue
@@ -1,22 +1,7 @@
 <template>
   <div>
-    <full-page :options="options">
+
       <slot />
-    </full-page>
+
   </div>
 </template>
-
-<script>
-
-  export default {
-
-    data() {
-      return {
-        options: {
-          paddingTop: '3.6rem',
-
-        },
-      }
-    },
-  }
-</script>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,17 +1,15 @@
 import VueSimpleAlert from "vue-simple-alert";
 import VueRellax from 'vue-rellax';
 
-
-export default ({
+export default ( {
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
+
 }) => {
 
 Vue.use(VueSimpleAlert);
 Vue.use(VueRellax);
-import('vue-fullpage.js').then(module => {
-    Vue.use(module.default)
-  })
+
 }
