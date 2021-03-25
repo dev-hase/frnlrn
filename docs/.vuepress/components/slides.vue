330733cab02e3081678b0277a01986203483c7fa
Daniel Schmidt <2wusel@web.de>
2021-03-25 12:52:08 +0100

    fullpage2

--- docs/.vuepress/components/slides.vue
+++ docs/.vuepress/components/slides.vue
@@ -1,13 +1,15 @@
 <template>
 
     <full-page>
+
       <div class="section">
         <slot />
         Section 1
       </div>
+
       <div class="section">
         Section 2
-        <div class="slide">
+            <div class="slide">
                 <h3>Slide 2.1</h3>
             </div>
             <div class="slide">
@@ -15,26 +17,20 @@
             </div>
             <div class="slide">
                 <h3>Slide 2.3</h3>
-        </div>
+            </div>
       </div>
+
       <div class="section">
         Section 3
       </div>
+
     </full-page>
 
 </template>
 
 <script>  
 export default {
-    props: ["infoImg", "secondImg"],
 
-    computed: {
-        isSecondImg: function () {
-            if (this.secondImg) return true
-            else return false
-        },
-        
-    }    
 
 };  
 </script>
